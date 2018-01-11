7718809196c803e260a9fe511a0164c10cea25c1
PJ Frias <peejfrias@gmail.com>
2018-01-11 21:33:53 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
+
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
 end
 
 # projects: kickstarter.css("li.project.grid_4")

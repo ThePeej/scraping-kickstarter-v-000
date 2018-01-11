fe6d1d717c3efecb5ff034d93305d20c9b37c303
PJ Frias <peejfrias@gmail.com>
2018-01-11 21:32:03 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,5 +14,6 @@ end
 # image: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css(".location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 create_project_hash
